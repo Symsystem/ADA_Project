fba7092da243b855b9242d42c4aaba722f676a7c
COLOMBO <pierre.colombo@supelec.fr>
2016-12-17 18:24:12 +0100

    beginning of D3.js

--- source/cleaning_data/function.py
+++ source/cleaning_data/function.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def get_datas(path) :
-    df_data=pd.read_csv(path,delimiter='\t',nrows=20,encoding='utf-8')
-    return  df_data
 
 ''' delete all the columns except :
 Name of the collum      Number
@@ -14,8 +11,26 @@ Name of the collum      Number
 - longitude             11
 - number of followers   17
 - number of friends     18
-- statusesCount         19
 - user location         20
 '''
-def delete_useless_columns(df_data) :
-    return
+
+map_columns = ['id','userId','date','content','longitude','latitude', 'placeId', 'inReplyTo','source','truncated','placeLatitude','placeLongitude'
+               , 'sourceName','sourceUrl','userName','screenName','followers','friends','statusesCount','userLocation']
+
+def get_datas(path,columns) :
+    index_columns  = []
+    for name in columns :
+        index_columns.append(map_columns.index(name))
+
+    df_data=pd.read_csv(path,delimiter='\t',nrows=9760,encoding='utf-8')[index_columns]
+    #
+    df_data.columns = columns
+    return  df_data
+
+def clean_data(data, columns_to_clean_na , columns_to_keep) :
+
+    for col in columns_to_clean_na :
+        data = data[data[col] != r'\N']
+    return data[columns_to_keep]
+
+
--- source/cleaning_data/main.py
+++ source/cleaning_data/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from ADA_Project.source.cleaning_data.function import get_datas
+from ADA_Project.source.cleaning_data.function import get_datas, clean_data
 
-df = get_datas('../../twitter-swisscom/sample.tsv')
-
+df = get_datas('../../twitter-swisscom/sample.tsv', ['id','date','content', 'latitude', 'longitude', 'followers','friends'])
+df = clean_data(df, [ 'longitude','latitude'],  ['id','date', 'latitude', 'longitude', 'followers','friends'])
